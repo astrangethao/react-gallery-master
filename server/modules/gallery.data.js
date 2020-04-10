6cbe5b743aff5d0aacf1161fadd75443888dc72a
Andrew Nguyen <anguyen1320@gmail.com>
2020-04-10 16:06:45 -0500

    created a get axios call and componentDidMount method; also added images to the gallery data; and created an empty database file

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,35 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+  {
+    id: 1,
+    title: "goat_small",
+    description: "Photo of a goat taken at Glacier National Park.",
+    path: "images/goat_small.jpg",
+    likes: 0,
+  },
+
+  {
+    id: 2,
+    title: "unicorn",
+    description: "Unicorn Digital Painting by James Jean.",
+    path: "images/unicorn.jpg",
+    likes: 1000,
+  },
+
+  {
+    id: 3,
+    title: "Passage",
+    description: "Passage Digital Painting by James Jean.",
+    path: "images/passage.jpg",
+    likes: 10000,
+  },
+
+  {
+    id: 4,
+    title: "Silver Arrow",
+    description: "Cool silver car.",
+    path: "images/silver_arrow.jpg",
+    likes: 0,
+  },
 ];
 
 module.exports = galleryItems;
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,47 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    galleryList: [],
+  };
+
+  componentDidMount() {
+    console.log("Component App did Mount");
+    this.getList();
+  }
+
+  getList() {
+    axios({
+      method: "GET",
+      url: "/gallery",
+    })
+      .then((responseDB) => {
+        console.log(responseDB.data);
+        this.setState(
+          {
+            galleryList: [...responseDB.data],
+          },
+          () => {
+            console.log("New List:", this.state.galleryList);
+          }
+        );
+      })
+      .catch((error) => {
+        console.warn(`There was an error getting List`, error);
+      });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of my life</h1>
         </header>
-        <br/>
+        <br />
         <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        <img src="images/goat_small.jpg" />
       </div>
     );
   }
